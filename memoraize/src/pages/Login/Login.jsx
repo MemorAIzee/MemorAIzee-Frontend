76a56d25a0379598aa55bd4632882ac4ae6238c5
이나경 <lnk5324@hansung.ac.kr>
2024-03-14 16:10:58 +0900

    ✨ Update Login

--- memoraize/src/pages/Login/Login.jsx
+++ memoraize/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import Header from '../../components/Header/Header';
 import styled from 'styled-components';
-
+import kakao from '../../assets/images/kakao.png';
 const Container = styled.div`
   display: flex;
   justify-content: center;
@@ -10,9 +10,9 @@ const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 60%;
-  background-color: yellow;
+  /* background-color: yellow; */
   margin-top: 4vw;
-  height: 20vw;
+  height: 40vw;
   align-items: center;
 `;
 
@@ -21,6 +21,7 @@ const InputContainer = styled.div`
   flex-direction: column;
   margin-top: 2.9vw;
   gap: 1.5vw;
+  margin-bottom: 1vw;
 `;
 
 const InputField = styled.input`
@@ -29,6 +30,16 @@ const InputField = styled.input`
   border-radius: 5px;
   border: 1px solid var(--Gray-Gray-300, #d1d1d1);
   padding-left: 0.8vw;
+  font-size: 1vw;
+
+  ::placeholder {
+    color: var(--6, #414141);
+    font-family: Pretendard;
+    font-size: 1vw;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
 `;
 
 const LoginText = styled.p`
@@ -37,7 +48,71 @@ const LoginText = styled.p`
 `;
 
 const FindText = styled.p`
+  color: var(--6, #414141);
+  font-family: Pretendard;
   font-size: 0.7vw;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const LoginBtn = styled.button`
+  margin-top: 2vw;
+  width: 40vw;
+  height: 3vw;
+  flex-shrink: 0;
+  color: #fff;
+  font-family: Pretendard;
+  font-size: 0.9vw;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  border-radius: 0.5vw;
+  background: var(--MAIN, #0034ed);
+  margin-bottom: 0.8vw;
+`;
+
+const JoinBtn = styled.button`
+  width: 40vw;
+  height: 3vw;
+  flex-shrink: 0;
+  border-radius: 0.5vw;
+  border: 1px solid var(--MAIN, #0034ed);
+  color: #000;
+  font-family: Pretendard;
+  font-size: 0.9vw;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+const Simplecontainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 2vw;
+`;
+
+const SimpleStroke = styled.div`
+  width: 16vw;
+  height: 0px;
+  flex-shrink: 0;
+  stroke-width: 1px;
+  stroke: var(--2, #902a2a);
+`;
+
+const KakaoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2vw;
+  margin-top: 1.2vw;
+`;
+const SimpleText = styled.p`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 0.8vw;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
 `;
 const Login = () => {
   return (
@@ -50,6 +125,18 @@ const Login = () => {
             <InputField placeholder="아이디를 입력해주세요"></InputField>
             <InputField placeholder="비밀번호를 입력해주세요"></InputField>
           </InputContainer>
+          <FindText>아이디 찾기 | 비밀번호 찾기</FindText>
+          <LoginBtn>로그인하기</LoginBtn>
+          <JoinBtn>회원가입하기</JoinBtn>
+          <Simplecontainer>
+            <SimpleStroke />
+            <SimpleText>간편 로그인</SimpleText>
+            <SimpleStroke />
+          </Simplecontainer>
+          <KakaoContainer>
+            <img src={kakao} style={{ width: '2vw', height: '2vw' }} />
+            <img src={kakao} style={{ width: '2vw', height: '2vw' }} />
+          </KakaoContainer>
         </LoginContainer>
       </Container>
     </>
