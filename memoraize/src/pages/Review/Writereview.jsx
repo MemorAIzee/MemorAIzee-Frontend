90eab1a7fd45c4eed080b6f9da5453d1dcdc6e4d
이나경 <lnk5324@hansung.ac.kr>
2024-03-25 09:10:44 +0900

    Update Writereview.jsx

--- memoraize/src/pages/Review/Writereview.jsx
+++ memoraize/src/pages/Review/Writereview.jsx
@@ -29,9 +29,20 @@ const ContentContainer = styled.div`
   flex-direction: row;
   gap: 8.05vw;
   width: 100%;
+  align-items: center;
 `;
 
 const ContentP = styled.p`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 1.8vw;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+const Contenttitle = styled.p`
+  margin-bottom: 1.7vw;
   color: #000;
   font-family: Pretendard;
   font-size: 1.8vw;
@@ -48,6 +59,41 @@ const Titlefield = styled.input`
   border: 1px solid #e1e1e1;
   background: #f7f7f7;
 `;
+
+const RequireText = styled.p`
+  text-align: left;
+  margin-top: 1vw;
+  color: var(--5, #707070);
+  font-family: Pretendard;
+  font-size: 0.8vw;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+const FieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const ReviewContentContainer = styled.div`
+  margin-top: 2vw;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+`;
+
+const ReviewP = styled.p`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 1.8vw;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
 const Writereview = () => {
   return (
     <>
@@ -58,14 +104,22 @@ const Writereview = () => {
 
       <Container>
         <CreatesContainer>
-          <ContentContainer>
+          <ContentContainer style={{ marginBottom: '4.2vw' }}>
             <ContentP>Rating</ContentP>
             <img src={Star} style={{ width: '12.2vw', height: '1.8vw' }} />
           </ContentContainer>
-          <ContentContainer style={{ gap: '9.45vw' }}>
-            <ContentP>Title</ContentP>
-            <Titlefield placeholder="제목을 입력해주세요"></Titlefield>
+
+          <ContentContainer style={{ gap: '9.45vw', alignItems: 'center' }}>
+            <Contenttitle>Title</Contenttitle>
+            <FieldContainer>
+              <Titlefield placeholder="제목을 입력해주세요" />
+              <RequireText>* 최대 50자 이내</RequireText>
+            </FieldContainer>
           </ContentContainer>
+
+          <ReviewContentContainer>
+            <ReviewP>Content</ReviewP>
+          </ReviewContentContainer>
         </CreatesContainer>
       </Container>
     </>
